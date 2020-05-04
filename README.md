94b28372e02b6511e935e65925059ac514632a62
sausix <aur@sausenthaler.de>
2020-05-04 12:12:38 +0200

    first cleanups

--- README.md
+++ README.md
@@ -69,26 +69,26 @@ sudo make flash
 |	value	|	commandbyte	|	in hex	|	response	|	format		|
 |---------------|	----------	|--------------	|	----------	|	----------	|
 |A0 (GD0 CC1101)|	0b00000000	|	0x00	|	10bit in 2 Byte	|	0 -1023		|
-|A1 (MICS CO)	|	0b00000001	|	0x01	|	10bit in 2 Byte	|	0 -1023		|
-|A2 (MICS No2)	|	0b00000010	|	0x02	|	10bit in 2 Byte	|	0 -1023		|
-|A3 (MICS NH3	|	0b00000011	|	0x03	|	10bit in 2 Byte	|	0 -1023		|
-|A4 (MP135)	|	0b00000100	|	0x04	|	10bit in 2 Byte	|	0 -1023		|
-|A5 (NTC 10K)	|	0b00000101	|	0x05	|	10bit in 2 Byte	|	0 -1023		|
-|A7 (ACS712)	|	0b00000110	|	0x06	|	10bit in 2 Byte	|	0 -1023		|
-|BL_LEVEL	|	0b00000111	|	0x07	|	1 Byte		|	0-31		|
-|VENT_RPM	|	0b00001000	|	0x08	|	2 Byte		|	UPM		|
-|VCC Atmega	|	0b00001001	|	0x09	|	2 Byte		|	in Millivolts	|
-|IntTemp Atmega	|	0b00001010	|	0x0A	|	2 Byte		|	degree celsius	|
-|FREERAM	|	0b00001011	|	0x0B	|	2 Byte		|	in Bytes	|
-|LED_COLOR	|	0b00001100	|	0x0C	|	3 Byte		|	R, G, B 0-255	|
-|Relay1		|	0b00001101	|	0x0D	|	1 Byte		|	0x00 / 0xFF	|
-|Relay2		|	0b00001110	|	0x0E	|	1 Byte		|	0x00 / 0xFF	|
-|Relay3		|	0b00001111	|	0x0F	|	1 Byte		|	0x00 / 0xFF	|
+|A1 (MICS CO)   |	0b00000001	|	0x01	|	10bit in 2 Byte	|	0 -1023		|
+|A2 (MICS No2)  |	0b00000010	|	0x02	|	10bit in 2 Byte	|	0 -1023		|
+|A3 (MICS NH3   |	0b00000011	|	0x03	|	10bit in 2 Byte	|	0 -1023		|
+|A4 (MP135)	    |	0b00000100	|	0x04	|	10bit in 2 Byte	|	0 -1023		|
+|A5 (NTC 10K)   |	0b00000101	|	0x05	|	10bit in 2 Byte	|	0 -1023		|
+|A7 (ACS712)    |	0b00000110	|	0x06	|	10bit in 2 Byte	|	0 -1023		|
+|BL_LEVEL       |	0b00000111	|	0x07	|	1 Byte		|	0-31		|
+|VENT_RPM       |	0b00001000	|	0x08	|	2 Byte		|	UPM		|
+|VCC Atmega	    |	0b00001001	|	0x09	|	2 Byte		|	in Millivolts	|
+|IntTemp Atmega |	0b00001010	|	0x0A	|	2 Byte		|	degree celsius	|
+|FREERAM        |	0b00001011	|	0x0B	|	2 Byte		|	in Bytes	|
+|LED_COLOR      |	0b00001100	|	0x0C	|	3 Byte		|	R, G, B 0-255	|
+|Relay1         |	0b00001101	|	0x0D	|	1 Byte		|	0x00 / 0xFF	|
+|Relay2         |	0b00001110	|	0x0E	|	1 Byte		|	0x00 / 0xFF	|
+|Relay3         |	0b00001111	|	0x0F	|	1 Byte		|	0x00 / 0xFF	|
 |D13 (reset RPI)|	0b00010000	|	0x10	|	1 Byte		|	0x00 / 0xFF	|
 |HWB (gasheater)|	0b00010001	|	0x11	|	1 Byte		|	0x00 / 0xFF	|
-|Buzzer		|	0b00010010	|	0x12	|	1 Byte		|	0x00 / 0xFF	|
-|VENT_PWM	|	0b00010011	|	0x13	|	1 Byte		|	0 – 255		|
-|A7_VG(AC curr) |       0b00010110      |       0x14    |       1 Byte          |       0 – 1023         |
+|Buzzer         |	0b00010010	|	0x12	|	1 Byte		|	0x00 / 0xFF	|
+|VENT_PWM       |	0b00010011	|	0x13	|	1 Byte		|	0 – 255		|
+|A7_VG(AC curr) |	0b00010110	|	0x14	|	1 Byte		|	0 – 1023	|
 
 										
 ## I2C write commands									
--- main.c
+++ main.c
@@ -1,133 +1,60 @@
 /*
-*  SHPI.zero Basic Firmware v1.2 BETA 
-*  
-* 
+* SHPI.zero Basic Firmware v1.2 BETA
+*
+* Author: Lutz Harder, Adrian Sausenthaler
+* License: GNU GPL
 *
 * Basic Firmware for ATmega32u4 slave,  no radio module support included
 *
-* Jul    15th, 2019  v 1.1b initial Version
-
-* October 4th, 2019  v1.2  Beta
+* Jul    15th, 2019  [00] v 1.1b initial Version (LH)
+*
+* October 4th, 2019  [00] v 1.2  Beta (LH)
 *    NEW: implemented CRC Check via CRC8  CCITT
 *
+* May    04th, 2020  [01] v 2.0  Beta (AS)
+*    NEW: Flash-Version check. Speed improvements.
 *
-*   I2C Command Reference     (SLAVE ADDRESS 0x2A)
-*
-*   ACTIVE COMMANDS
-*
-*   0x87 set backlight level of LCD   0-31  // i2cset -y 2 0x2A 0x87 31 +CRC
-*OLD:  0x8C  set RGB value of LED              // i2cset -y 2 0x2A 0x8C 0xRR 0xGG 0xBB +CRC i         (replace RR GG BB by value 0-255)
-*   0x8D  set Relay 1                       // i2cset -y 2 0x2A 0x8D 0x00 +CRC                     (0x00 = off  0xFF = on)
-*   0x8E  set Relay 2                       // i2cset -y 2 0x2A 0x8E 0x00 +CRC                    (0x00 = off  0xFF = on)
-*   0x8F  set Relay 3                       // i2cset -y 2 0x2A 0x8F 0x00 +CRC                    (0x00 = off  0xFF = on)
-*   0x90  set D13 / PC7                     // i2cset -y 2 0x2A 0x90 0x00 +CRC                    (0x00 = off  0xFF = on)
-*   0x91  set HWB / PE2                     // i2cset -y 2 0x2A 0x91 0x00 +CRC                    (0x00 = off  0xFF = on)
-*   0x92  set Buzzer / PB5                  // i2cset -y 2 0x2A 0x92 0x00 +CRC                    (0x00 = off, 0x01 on for click sound,0xFF = on)
-*   0x93  set Vent power                    // i2cset -y 2 0x2A 0x93 0x00 +CRC                    (PWM:  0x00 = on ... 0xFF = off)
-*   0x94  set R color
-*   0x95  set G color
-*   0x96  set B color 
-*
-*
-*  READOUT VALUES
-*
-*
-*   0x00  read A0                           // i2cset -y 2 0x2A 0x00                    
-*                                           // i2cget -y 2 0x2A         -> frst low byte
-*                                           // i2cget -y 2 0x2A         -> scnd high byte
-*
-*   0x01  read A1 
-*   0x02  read A2 
-*   0x03  read A3 
-*   0x04  read A4 
-*   0x05  read A5 
-*   0x06  read A7 
-*
-*   0x07  read actual backlight level       // i2cget -y 2 0x2A 0x07
-*                                             -> only one byte response
-*
-*
-*   0x08  read actual RPM von vent         // i2cset -y 2 0x2A 0x08
-*                                          // i2cget -y 2 0x2A         -> frst low byte
-*                                          // i2cget -y 2 0x2A         -> scnd high byte
-*
-*   0x09 read supply voltage of ATmega     // i2cset -y 2 0x2A 0x09                    
-*                                          // i2cget -y 2 0x2A         -> frst low byte
-*                                          // i2cget -y 2 0x2A         -> scnd high byte
-*
-*
-*
-*
-*   0x0A read internal ATmega temp         // i2cset -y 2 0x2A 0x0A                    
-*                                          // i2cget -y 2 0x2A         -> frst low byte
-*                                          // i2cget -y 2 0x2A         -> scnd high byte
-*
-*
-*   0x0B read free RAM ATmega              // i2cset -y 2 0x2A 0x0B                    
-*                                          // i2cget -y 2 0x2A         -> frst low byte
-*                                          // i2cget -y 2 0x2A         -> scnd high byte
-*
-*
-*   0x0C read RGB values from LED           // i2cset -y 2 0x2A 0x0C                    
-*                                          // i2cget -y 2 0x2A         -> red byte
-*                                          // i2cget -y 2 0x2A         -> green byte
-*                                          // i2cget -y 2 0x2A         -> blue byte
-*
-*
-*                                         
-*   0x0D  read relay 1                      // i2cget -y 2 0x2A 0x0D 
-*   0x0E  read relay 2                      // i2cget -y 2 0x2A 0x0E 
-*   0x0F  read relay 3                      // i2cget -y 2 0x2A 0x0F 
-*   0x10  read D13                         // i2cget -y 2 0x2A 0x10 
-*   0x11  read HWB                         // i2cget -y 2 0x2A 0x11 
-*   0x12  read Buzzer                      // i2cget -y 2 0x2A 0x12 
-*   0x13  read VENT PWM value              // i2cget -y 2 0x2A 0x13    
-*   0x14  read AVG A7 for AC currents 
-*
-*
-*
-*
-*
-* Author: Lutz Harder
-* License: GNU GPL
 */
+#define FW_VERSION  0x01
+#define I2C_ADDR 0x2A
+
 
 #include <util/crc16.h>
 #include <avr/wdt.h>
 #include <avr/power.h>
 #include <avr/io.h>
-#include <stdlib.h>
-#define DELAY 100
+#define LCD_WRITE_DELAY 100
 #define WAIT 120
 #include <util/delay.h>
 #include <avr/interrupt.h>
 #include <stdint.h>
 #include <util/twi.h>
-#define ws2812_resettime  300 
-#define ws2812_port D   
-#define ws2812_pin  5   
+
+#define ws2812_resettime 300
+#define ws2812_port D
+#define ws2812_pin 5
 #include "light_ws2812.c"
-#include "light_ws2812.h"
 struct cRGB led[1];
-#define I2C_ADDR 0x2A
+
+
 #define SDA_LINE  (PIND & (1<<PD1))
 
-uint8_t commandbyte = 0xFF,twdrbuffer, buffer_address,a7count = 0,count,bllevel = 31,newbllevel = 31,changeled,crc,i2cerror = 0;  
-uint16_t a0,a1,a2,a3,a4,a5,a7,a7avg,a7max,a7min,vcc,temp,rpm,fanspin,isrtimer,i2cbuffer = 0;
-uint8_t fanlevel = 254;
+uint8_t commandbyte=0xFF, twdrbuffer, buffer_address, a7count=0, count, bllevel = 31, newbllevel = 31, changeled, crc, i2cerror=0;
+uint16_t a0, a1, a2, a3, a4, a5, a7, a7avg, a7max, a7min, vcc, temp, rpm, fanspin, isrtimer, i2cbuffer=0;
+uint8_t fanlevel=254;
 
 
+uint16_t data_lcd_A035VW01[] = {0x0080, 0x01F8,  0x0246, 0x0305, 0x0440,  0x0540, 0x0640,0x0740, 0x0840,  0x0940, 0x0A03};            // only for  A035VW01
 
-uint16_t commands[] =  {0x0080, 0x01F8,  0x0246, 0x0305, 0x0440,  0x0540, 0x0640,0x0740, 0x0840,  0x0940, 0x0A03};            // only for  A035VW01 
-uint16_t commands2[]=  {0x0011,0x0000,0x0001,0x0000,0x00C1,0x01A8,0x01B1,0x0145,0x0104,0x0C5,0x0180,0x016C,0x0C6,               // initcode for LCD A035VL01
+uint16_t data_lcd_A035VL01[] = {0x0011,0x0000,0x0001,0x0000,0x00C1,0x01A8,0x01B1,0x0145,0x0104,0x0C5,0x0180,0x016C,0x0C6,               // initcode for LCD A035VL01
                        0x01BD,0x0184,0x00C7,0x01BD,0x0184,0x00BD,0x0102,0x0011,0x0000,0x00F2,0x0100,0x0100,0x0182,
                        0x0026,0x0108,0x00E0,0x0100,0x0104,0x0108,0x010B,0x010C,0x0111,0x010D,0x010E,0x0100,0x0104,
                        0x0108,0x0113,0x0114,0x012F,0x0129,0x0124,0x00E1,0x0100,0x0104,0x0108,0x010B,0x010C,0x0111,
                        0x010D,0x010E,0x0100,0x0104,0x0108,0x0113,0x0114,0x012F,0x0129,0x0124,0x0026,0x0108,0x00FD,
                        0x0100,0x0108,0x0029};             
-            
 
-void writebl(uint8_t data) { // set single wire brightness  AL3050 
+
+void write_backlight(uint8_t data) { // set single wire brightness  AL3050
   uint8_t count = 8;
   do {
     PORTD &= ~_BV(PD4);
@@ -149,7 +76,7 @@ void writebl(uint8_t data) { // set single wire brightness  AL3050
   _delay_us(100);
 }
 
-void initbl() { // init AL3050 single wire dimming
+void init_backlight(void) { // init AL3050 single wire dimming
   PORTD &= ~_BV(PD4);
   _delay_us(3000);
   PORTD |= _BV(PD4);
@@ -160,15 +87,15 @@ void initbl() { // init AL3050 single wire dimming
   _delay_us(5);
 }
 
-void write(uint16_t data, uint8_t count) { //  write routine for LCD setup
+void write_lcd(uint16_t data, uint8_t count) { //  write routine for LCD setup
   PORTD &= ~_BV(PD4);
   do {
     PORTB &= ~_BV(PB2);
     PORTB |= (((data & (1 << (count - 1))) != 0) << 2); // BITWISE AND -> PB2           
     PORTB &= ~_BV(PB1);
-    _delay_us(DELAY);
+    _delay_us(LCD_WRITE_DELAY);
     PORTB |= _BV(PB1);
-    _delay_us(DELAY);
+    _delay_us(LCD_WRITE_DELAY);
     count--;
   } while (count);
   PORTB &= ~_BV(PB2);
@@ -176,20 +103,23 @@ void write(uint16_t data, uint8_t count) { //  write routine for LCD setup
 }
 
 void setup_lcd(void) {
-  for (int x = 0; x < 11; x++) {
-    write(commands[x], 16);
-  } //only for A035VW01
-  for (int x = 0; x < (sizeof(commands2) / sizeof(uint16_t)); x++) {
-    if (commands2[x] == 0x0000) {
-      _delay_ms(WAIT);
-      continue;
-    }
-    write(commands2[x], 9);
-  } //only for A035VL01
+	//only for A035VW01
+	for (int x=0; x<sizeof(data_lcd_A035VW01); x++) {
+		write_lcd(data_lcd_A035VW01[x], 16);
+	}
 
+
+	//only for A035VL01
+	for (int x=0; x<(sizeof(data_lcd_A035VL01) / sizeof(uint16_t)); x++) {
+		if (data_lcd_A035VL01[x] == 0x0000) {
+			_delay_ms(WAIT);
+			continue;
+		}
+		write_lcd(data_lcd_A035VL01[x], 9);
+	}
 }
 
-uint16_t readAna(uint8_t channel) {
+uint16_t read_analog(uint8_t channel) {
   uint8_t low, high;
   ADCSRA |= _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
   ADCSRB = 0x40;
@@ -197,7 +127,7 @@ uint16_t readAna(uint8_t channel) {
 
   if (channel >= 8) //
   {
-    channel -= 0x08; //ch - 8           
+    channel -= 0x08; // ch - 8
     ADCSRB |= (1 << MUX5); // set MUX5 on ADCSRB to read upper bit ADC8-ADC13
   } else {
     ADCSRB &= ~(1 << MUX5); // clear MUX 5 
@@ -213,7 +143,6 @@ uint16_t readAna(uint8_t channel) {
   low = ADCL;
   high = ADCH;
   return (high << 8) | low;
-
 }
 
 uint16_t readVcc(void) {
@@ -229,7 +158,6 @@ uint16_t readVcc(void) {
 }
 
 uint16_t GetTemp(void) {
-
   ADMUX = _BV(REFS1) | _BV(REFS0) | 7; // Set internal V reference, temperature reading
   ADCSRB = 0x20; // ref  24.6
   ADCSRA &= ~(_BV(ADATE) | _BV(ADIE)); // Clear auto trigger and interrupt enable
@@ -251,13 +179,14 @@ uint16_t freeRam(void) {
   return (uint16_t) & v - (__brkval == 0 ? (int) & __heap_start : (int) __brkval);
 }
 
+
 void I2C_init(uint8_t address) // setup ATmega as I2C slave
 {
   cli();
 
-  TWAR = (address << 1);
+  TWAR = (address << 1); // Slave address without general call.
   TWCR = (1 << TWEN) | // TWI Interface enabled.
-    (1 << TWIE) | (1 << TWINT) | // Enable TWI Interupt and clear the flag.
+    (1 << TWIE) | (1 << TWINT) | // Enable TWI interrupt and clear the flag.
     (1 << TWEA) | (0 << TWSTA) | (0 << TWSTO) | // Prepare to ACK next time the Slave is addressed.
     (0 << TWWC);
 
@@ -470,106 +399,113 @@ ISR(TWI_vect) {
 }
 
 
-void setup(void)
-{
-   DDRF = 0b00000000;
-   DDRD = 0b01111000;
-   PORTD= 0b00000000;
-   DDRE = 0b00000000;   // DDRE |= (1<<2);   be carefull with hwb, check if its connected to GND via 10k (prototypes!)
-   DDRB = 0b11110110;
-   DDRC = 0b11000000;
-   OCR0A = 0;           //    start value for FAN  0 / 255  (-> p-channel so inverted)       0x00 is ON  0xFF is OFF
-   TCCR0B  =  0b00000001;
-   TCCR0A  =  0b10000001;            // 8bit dual slope 31khz
-   TIMSK0 |= (1 << TOIE0);            // init interrupt for timer0 overflow
-   clock_prescale_set(clock_div_1);
-   I2C_init(I2C_ADDR);
-   PCICR |= _BV(PCIE0);              // enable pin change interrupt for PB0 (rpm)
-   PCMSK0 |= _BV(PCINT0);
-   sei();
-   led[0].r = 255;
-   led[0].g = 255;
-   led[0].b = 255;
-   ws2812_setleds(led,1);
-   setup_lcd();
-   initbl();
-   led[0].r = 0;
-   led[0].g = 0;
-   led[0].b = 0;
-   ws2812_setleds(led,1);
-   OCR0A = 210;
-   wdt_enable(WDTO_8S);
-   bllevel = 0;
-   newbllevel = 31;
+void setup(void) {
+	DDRF = 0b00000000;
+	DDRD = 0b01111000;
+	PORTD= 0b00000000;
+	DDRE = 0b00000000;   // DDRE |= (1<<2);   be carefull with hwb, check if its connected to GND via 10k (prototypes!)
+	DDRB = 0b11110110;
+	DDRC = 0b11000000;
+	OCR0A = 0;           //    start value for FAN  0 / 255  (-> p-channel so inverted)       0x00 is ON  0xFF is OFF
+	TCCR0B  =  0b00000001;
+	TCCR0A  =  0b10000001;            // 8bit dual slope 31khz
+	TIMSK0 |= (1 << TOIE0);            // init interrupt for timer0 overflow
+	clock_prescale_set(clock_div_1);
+	I2C_init(I2C_ADDR);
+	PCICR |= _BV(PCIE0);              // enable pin change interrupt for PB0 (rpm)
+	PCMSK0 |= _BV(PCINT0);
+	sei();
+	led[0].r = 255;
+	led[0].g = 255;
+	led[0].b = 255;
+	ws2812_setleds(led, 1);
+	setup_lcd();
+	init_backlight();
+	led[0].r = 0;
+	led[0].g = 0;
+	led[0].b = 0;
+	ws2812_setleds(led, 1);
+	OCR0A = 210;
+	wdt_enable(WDTO_8S);
+	bllevel = 0;
+	newbllevel = 31;
 }
 
 
-int main(void)
-{
-  uint8_t adcselect = 0;
-  setup();
+int main(void) {
+	uint8_t adcselect = 0;
+	setup();
 
-  while(1) {  
-
-  if (!SDA_LINE) {i2cerror++;}
+	while(1) {
+		if (!SDA_LINE) {
+			i2cerror++;
+		}
   
-  if (i2cerror > 50) {
-  TWCR =   (1<<TWSTO)|(1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (0<<TWEN); 
-  I2C_init(I2C_ADDR); 
-  i2cerror = 0;
-  sei();
-  }                    
+		if (i2cerror > 50) {
+			TWCR = (1<<TWSTO) | (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (0<<TWEN);
+			I2C_init(I2C_ADDR);
+			i2cerror = 0;
+			sei();
+		}
 
-  wdt_reset();	
-  if (isrtimer > 31250)   // routine for calculate fan speed - timer is 32khz
-  {rpm = fanspin * 30;    // 2 signals each turn        
-  fanspin = 0;
-  isrtimer = 0;
-  if (fanlevel == 254) { //fan minimal auto
-  if (rpm > 4400) {OCR0A++;}
-  if (rpm < 3600) {OCR0A--;}
-  }
-  }
+		wdt_reset();
+		if (isrtimer > 31250) {  // routine for calculate fan speed - timer is 32khz
+			rpm = fanspin * 30;    // 2 signals each turn
+			fanspin = 0;
+			isrtimer = 0;
+			if (fanlevel == 254) { //fan minimal auto
+				if (rpm > 4400) {
+					OCR0A++;
+				}
+				if (rpm < 3600) {
+					OCR0A--;
+				}
+			}
+		}
 
-  if (changeled)  {
-      ws2812_setleds(led,1);
-      changeled = 0;
-                     }
+		if (changeled)  {
+			ws2812_setleds(led,1);
+			changeled = 0;
+		}
 
-  if (0 <= newbllevel && newbllevel < 32) {
-  if (newbllevel < bllevel) {                // smooth backlight level change in steps
+		if (0 <= newbllevel && newbllevel < 32) {
+			if (newbllevel < bllevel) {                // smooth backlight level change in steps
+				bllevel--;
+				write_backlight(0b01011000);
+				write_backlight(0b00011111 & bllevel);
+			}
 
-      bllevel--;
-      writebl(0b01011000);   writebl(0b00011111 & bllevel);
-	  }
-
-     if (newbllevel > bllevel) {
-
-	  bllevel++;
-	   writebl(0b01011000);   writebl(0b00011111 & bllevel);
-
-	  }}
+			if (newbllevel > bllevel) {
+				bllevel++;
+				write_backlight(0b01011000);
+				write_backlight(0b00011111 & bllevel);
+			}
+		}
 
   if (adcselect < 10) {adcselect++;} else {adcselect = 0;}
   
   switch(adcselect)
   {
-   case 0: a0 = readAna(7);  break;
-   case 1: a1 = readAna(6);  break;
-   case 2: a2 = readAna(5);  break;
-   case 4: a3 = readAna(4);  break; 
-   case 5: a4 = readAna(1);  break; 
-   case 7: a5 = readAna(0);  break;
+   case 0: a0 = read_analog(7);  break;
+   case 1: a1 = read_analog(6);  break;
+   case 2: a2 = read_analog(5);  break;
+   case 4: a3 = read_analog(4);  break;
+   case 5: a4 = read_analog(1);  break;
+   case 7: a5 = read_analog(0);  break;
    case 8: vcc = readVcc();  break;
    case 10: temp = GetTemp();   break;
 
  
-   default: {a7 = readAna(10);  //read A7 more frequently 
-           if (a7 > a7max) a7max = a7;
-           if (a7 < a7min) a7min = a7;
+   default: {a7 = read_analog(10);  //read A7 more frequently
+           if (a7 > a7max)
+        	   a7max = a7;
+           if (a7 < a7min)
+        	   a7min = a7;
            a7count++;
            if (a7count > 60) {a7avg = (a7max - ((a7max +  a7min)/ 2)) * 0.707 ; a7min = 1024; a7max = 0; a7count = 0;} 
            break; 
            }
 
-}}}
+}
+  }
+}
